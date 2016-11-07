15f9f949f92bff3b5132b93471a48c077e3d6c34
Bruno <bruno@iset.com.br>
2016-11-07 17:30:00 -0200

    update rotas

--- public/index.php
+++ public/index.php
@@ -20,6 +20,7 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
     return $response;
 });
 
+// Retorna um JSON com os clientes cadastrados no banco com suas respectivas Rds e Stacks
 
 $app->get('/clientes/dashboard', function (Request $request , Response $response){
 
@@ -33,7 +34,9 @@ return $response;
 
 });
 
-$app->get('/rds',function (Request $request, Response $response){
+// Retorna um JSON com a lista de RDS
+
+$app->get('/rds/',function (Request $request, Response $response){
 
 $rds = new Rds();
 
@@ -45,7 +48,9 @@ return $response;
 
 });
 
-$app->get('/stack',function (Request $request, Response $response){
+// Retorna um JSON com a lista de Stacks
+
+$app->get('/stack/',function (Request $request, Response $response){
 
 $stack = new Stack();
 
@@ -57,10 +62,39 @@ return $response;
 
 });
 
-$app->post('/addrds', 'insertRds');
+$app->post('/rds/' ,function (Request $request, Response $response){
+
+$rds = new Rds();
+
+$nome = $app->request->post('nome');
+var_dump($nome);
+die();
+$id = $app->request->post('id');
+$url = $app->request->post('url');
+$port = $app->request->post('port');
+
+
+$rds->setNome($nome); 
+$rds->setNome($id); 
+$rds->setNome($url); 
+$rds->setNome($port); 
+
+$cadastro = $rds->cadastrarRds();
+
+$response = json_encode($cadastro);
+
+return $response;
+
+});
+
+
 $app->post('/addstack', 'insertStack');
+
+
 $app->post('/updaterds', 'updateRds');
 $app->post('/updatestack', 'updateStack');
+
+
 $app->delete('/deleterds/delete/:update_id','deleteUpdate');
 $app->delete('/deletestack/delete/:update_id','deleteUpdate');
 
